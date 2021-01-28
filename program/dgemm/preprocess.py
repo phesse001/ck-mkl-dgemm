108ac9c33fad629772572c1d755ec3402b015e7e
Patrick Hesse <phesse001@csbsju.edu>
2021-01-27 18:06:15 -0600

    first commit

---
+++ program/dgemm/preprocess.py
@@ -0,0 +1,19 @@
+#
+# Preprocessing CK template demo
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, 2018, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import json
+import os
+import re
+
+def ck_preprocess(i):
+    print(i)
+
+    return {'return':0}
+
+# Do not add anything here!
